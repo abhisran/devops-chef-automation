a85bd29178e5c9b375eaa1e611b27c2a910e0141
Abhishek Ranjan <101317594+abhisran@users.noreply.github.com>
2026-02-01 18:27:13 +0530

    Bump k8s-master and k8s-worker cookbook versions to 0.1.2
    - Reflects idempotency fixes and cookstyle corrections

--- cookbooks/kubernetes/k8s-worker/metadata.rb
+++ cookbooks/kubernetes/k8s-worker/metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Abhishek Ranjan'
 maintainer_email 'abhisran60@gmail.com'
 license 'All Rights Reserved'
 description 'Installs/Configures Kubernetes worker node'
-version '0.1.0'
+version '0.1.2'
 chef_version '>= 16.0'
 
 depends 'apt'
