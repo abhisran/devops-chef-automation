29a595a9dd45dd33eceb538efcf5f6c5f5377457
Abhishek Ranjan <101317594+abhisran@users.noreply.github.com>
2026-02-01 23:42:03 +0530

    Remove apt cookbook dependency from k8s cookbooks (fixes Chef 18 NoMethodError)
    - apt_update is a built-in resource in Chef 16+, no external cookbook needed
    - Removes conflict causing NoMethodError with apt_update resource
    - Bumps versions to 0.1.3

--- cookbooks/kubernetes/k8s-worker/metadata.rb
+++ cookbooks/kubernetes/k8s-worker/metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Abhishek Ranjan'
 maintainer_email 'abhisran60@gmail.com'
 license 'All Rights Reserved'
 description 'Installs/Configures Kubernetes worker node'
-version '0.1.2'
+version '0.1.3'
 chef_version '>= 16.0'
 
-depends 'apt'
+# Note: apt_update is a built-in resource in Chef 16+, no apt cookbook needed
