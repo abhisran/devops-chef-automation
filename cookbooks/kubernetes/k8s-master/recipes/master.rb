3a285d2cc2090bc1324267a4b1dd60402f819c58
Abhishek Ranjan <101317594+abhisran@users.noreply.github.com>
2026-02-01 18:19:55 +0530

    Fix idempotency issues in k8s-master and k8s-worker cookbooks
    - Add guards to kubeadm init and kubeadm config images pull (skip if cluster initialized)
    - Add guards to copy-kube-config (only copy if admin.conf exists and config missing)
    - Add guards to install-network-plugin (only run if kube config exists)
    - Add guards to hold-k8s-packages in both master and worker (skip if already held)

--- cookbooks/kubernetes/k8s-master/recipes/master.rb
+++ cookbooks/kubernetes/k8s-master/recipes/master.rb
@@ -4,14 +4,16 @@
 #
 # Copyright:: 2025, The Authors, All Rights Reserved.
 
-# Pull required images first
+# Pull required images first (skip if cluster already initialized)
 execute 'kubeadm config images pull' do
   live_stream false
+  not_if { ::File.exist?('/etc/kubernetes/admin.conf') }
 end
 
-# Initialize Kubernetes master
+# Initialize Kubernetes master (skip if already initialized)
 execute 'kubeadm init' do
   live_stream false
+  not_if { ::File.exist?('/etc/kubernetes/admin.conf') }
 end
 
 # Configure kubectl for the root user
@@ -21,11 +23,16 @@ directory '/root/.kube' do
   mode '0755'
 end
 
-execute 'cp -i /etc/kubernetes/admin.conf /root/.kube/config' do
+execute 'copy-kube-config' do
+  command 'cp /etc/kubernetes/admin.conf /root/.kube/config'
   live_stream false
+  only_if { ::File.exist?('/etc/kubernetes/admin.conf') }
+  not_if { ::File.exist?('/root/.kube/config') }
 end
 
-# Install Weave network plugin
-execute "kubectl apply -f #{node['kubernetes']['network_plugin']['url']}" do
+# Install Weave network plugin (kubectl apply is idempotent)
+execute 'install-network-plugin' do
+  command "kubectl apply -f #{node['kubernetes']['network_plugin']['url']}"
   live_stream false
+  only_if { ::File.exist?('/root/.kube/config') }
 end
