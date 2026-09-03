4ae8f143c03bfb46b31ec2831da5b16f5e52d1a4
Abhishek Ranjan <101317594+abhisran@users.noreply.github.com>
2026-09-03 23:27:20 +0530

    feat(jenkins-server): upgrade jenkins version to 2.568.3 and bump cookbook version to 0.2.6

--- cookbooks/jenkins/jenkins-server/attributes/default.rb
+++ cookbooks/jenkins/jenkins-server/attributes/default.rb
@@ -1,5 +1,5 @@
 default['jenkins']['java_package'] = 'openjdk-21-jre'
-default['jenkins']['jenkins_version'] = '2.555.1'
+default['jenkins']['jenkins_version'] = '2.568.3'
 default['jenkins']['port'] = 8080
 default['jenkins']['java_args'] = '-Xmx1024m -Xms512m'
 default['jenkins']['home'] = '/var/lib/jenkins'
